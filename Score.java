629baae519311178a5663a2f8492fb88b8716270
Stephen <Swilson2999@gmail.com>
2016-11-08 14:55:15 -0500

    Amended the Score class with method starters and javadoc. (Hopefully didn't mess it up)  ~Stephen

--- Score.java
+++ Score.java
@@ -1,5 +1,45 @@
 package Group_Project_Bork;
 
-public class Score {
+import java.util.Hashtable;
 
+/**
+ * @author Stephen Well Son
+ * @version 11/8/2016.
+ */
+
+public class Score {
+int points;
+    private Hashtable<int, String> rank;
+
+    Score (){
+
+    }
+
+    /**
+     *setRank takes points to calculate a rank.
+     * @param rank takes the Hashtable rank.
+     * @param points takes the points integer.
+     */
+    public Hashtable setRank(int points, Hashtable rank){
+        this.rank = rank;
+        return rank;
+    }
+
+    /**
+     * getRank takes in Score to get the rank.
+     * @param Score takes the Score int.
+     * @return
+     */
+    public String getRank(int Score){
+
+    }
+
+/**
+ * getScore takes the points integer to calculate the score.
+ * and also returns the score
+ *@param points takes the points integer.
+ * */
+    public int getScore(int points, int score){
+
+    }
 }
