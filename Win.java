c752a67859c5039157bcc47e1660f8fca4a88204
centuryorder <yohan_selv@hotmail.com>
2016-11-08 11:17:09 -0500

    Corrected more format in Win.java

--- Win.java
+++ Win.java
@@ -12,17 +12,26 @@ public class Win {
     public Win(Win win){
 
     }
-    /** @param won  bossdefeated and @param score are taken in as arguments to calculate a win.  A certain score must be
-    // achieved.  the boss must be defeated for bossdefeated to return true, and the score will be taken and returned with win results. @Stephen
-    **/
+    /**
+     * hasWon take in won bossdefeated and score to calculate a win.  A certain score must be
+     * achieved.  the boss must be defeated for bossdefeated to return true,
+     * and the score will be taken and returned with win results. 
+     * @param won
+     * @param bossdefeated
+     * @param score
+     * @return
+     */
     public boolean hasWon(boolean won, boolean bossdefeated, int score){
-        return hasWon;
+        return false;
 
     }
     /**
-     * @param endGame takes hasWon as an argument to determine whether the game should keep running or not (win = close/true
-     *                otherwise keep running/ false @Stephen
-     * **/
+     * endGame takes hasWon as an argument to determine whether the game should keep running or not (win = close/true
+     *  otherwise keep running/ false
+     * @param hasWon
+     * @return
+     */
 public boolean endGame(boolean hasWon){
+	return false;
 
 }}
