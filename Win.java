ce5bc8824b9738d38fd9f9b87183376c26327af8
Stephen <Swilson2999@gmail.com>
2016-11-07 23:46:59 -0500

    Added some preliminary javadoc ish comments for win class. ~Stephen/Swilson

---
+++ Win.java
@@ -0,0 +1,27 @@
+package hendrawan_borkv3;
+/**
+ * Created by Stephen Well Son on 11/7/2016.
+ */
+// Win
+public class Win {
+    boolean won;
+    boolean bossdefeated;
+    int score;
+
+    public Win(Win win){
+
+    }
+    /** @param won  bossdefeated and @param score are taken in as arguments to calculate a win.  A certain score must be
+    // achieved.  the boss must be defeated for bossdefeated to return true, and the score will be taken and returned with win results. @Stephen
+    **/
+    public boolean hasWon(boolean won, boolean bossdefeated, int score){
+        return hasWon;
+
+    }
+    /**
+     * @param endGame takes hasWon as an argument to determine whether the game should keep running or not (win = close/true
+     *                otherwise keep running/ false @Stephen
+     * **/
+public boolean endGame(boolean hasWon){
+
+}}
