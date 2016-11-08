5db08d47cf392ce1762a0347e886b6b44415989b
Stephen <Swilson2999@gmail.com>
2016-11-08 15:18:48 -0500

    Started skeleton of Die class with hopefully not botched Java doc.  ~Swilson

---
+++ Die.java
@@ -0,0 +1,43 @@
+/**
+ * /**
+ * @author Stephen Well Son
+ * @version 11/8/2016.
+ */
+
+public class Die {
+boolean dead = false;
+
+    /**
+     * Die simply returns whether dead is true or not
+     * @param dead
+     */
+    public Die(boolean dead){
+      dead= false;
+    }
+    /**
+     * getScore takes the points integer to calculate the score.
+     * and also returns the score
+     *@param points takes the points integer.
+     * */
+    public int getScore(int points, int score){
+
+    }
+
+    /**
+     * removeNpc removes npc based on npc's hp
+     * @return
+     */
+    public NPC removeNpc(){
+
+    }
+
+    /**
+     * endGame takes dead as an argument to determine whether the game should keep running or not (dead = close/true/)
+     *  otherwise keep running/ false
+     * @param dead take in dead
+     * @return return a boolean value
+     */
+    public boolean endGame(boolean dead){
+        return false;
+
+}}
