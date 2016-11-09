2e01d7dc9331bd9a7c82744d252c6acab15e4314
Stephen <Swilson2999@gmail.com>
2016-11-09 17:56:17 -0500

    Javadoc and skeleton for crafting command

---
+++ CraftingCommand.java
@@ -0,0 +1,26 @@
+package Group_Project_Bork;
+/**
+ * @author Stephen Well Son
+ * @version 11/7/2016.
+ */
+public class CraftingCommand extends command {
+    String noun;
+    String verb;
+    String target;
+
+    /**
+     * CraftingCommand allows for player to transform items into new items based on strings noun verb and target.
+     * @param noun takes in the string noun
+     * @param verb takes in the string verb
+     * @param target takes in the string target.
+     */
+    public CraftingCommand (String noun, String verb, String target)
+{
+
+
+}
+
+public String Execute(){}
+
+
+}
