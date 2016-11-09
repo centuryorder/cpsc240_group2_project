14d502b498698c6af9dda022865a1f87512600ee
Stephen <Swilson2999@gmail.com>
2016-11-09 18:22:30 -0500

    Small javadoc and skeleton for disappear

---
+++ Disappear.java
@@ -0,0 +1,17 @@
+package Group_Project_Bork;
+
+/**
+ * @author Stephen Well Son
+ * @version 11/9/2016.
+ */
+public class Disappear extends Group_Project_Bork.Item {
+    Item item;
+
+    /**
+     * Disappear will handle dismissing items from the running instance of the program.
+     * @param item will be implemented
+     */
+    public Disappear(){
+
+    }
+}
