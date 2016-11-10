d365f74cd82e59f67fd0b11ec10f0b8ff5db7c27
Stephen <Swilson2999@gmail.com>
2016-11-09 19:03:38 -0500

    Small javadoc and skeleton for RoomItemSpecificCommand

---
+++ RoomItemSpecificCommand.java
@@ -0,0 +1,27 @@
+package Group_Project_Bork;
+
+/**
+ * Created by Stephen Well Son on 11/9/2016.
+ */
+public class RoomItemSpecificCommand extends Command {
+    String verb;
+    String target;
+
+    /**
+     * RoomItemSpecificCommand takes noun verb and target to deal with items only found in a specific room.
+     * @param noun
+     * @param verb
+     * @param target
+     * @return
+     */
+    public String RoomItemSpecificCommand(String noun, String verb, String target,){
+
+    }
+
+    /**
+     * Execute activates the action associated with the RoomItemSpecificCommand.
+     * @return
+     */
+    public String Execute(){}
+
+}
