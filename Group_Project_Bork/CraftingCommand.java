773ecb4d24a437c2f683506aa21d8fa30a8728f1
Stephen <Swilson2999@gmail.com>
2016-11-21 22:04:13 -0500

    Wrote up transform command

--- Group_Project_Bork/CraftingCommand.java
+++ Group_Project_Bork/CraftingCommand.java
@@ -17,15 +17,45 @@ public class CraftingCommand extends Command {
 	 */
 	public CraftingCommand (String noun, String verb, String target)
 	{
+		this.verb = verb;
+		this.noun = noun;
+		this.target = target;
 
 
 	}
+
 	/**
 	 * Triggers transform and disappears on an item
 	 */
-	public String execute() {
-		return null;
+	public String execute() {{
+		String msg ="Can't do that.";
+		Item tempI;
+		try {
+			tempI = GameState.instance().getItemFromInventoryNamed(noun);
+			Item tempV= GameState.instance().getItemInVicinityNamed(noun);
+			if (verb.equals("transform"))
+			{
+				if(tempV != null)
+				{
+					msg = tempV.getMessageForVerb(verb);
+					GameState.instance().getAdventurersCurrentRoom().remove(tempV);
+				}
+				else if(tempI != null)
+				{
+					msg = tempI.getMessageForVerb(verb);
+					GameState.instance().removeFromInventory(tempI);
+				}
+			}
+
+
+		} catch (Item.NoItemException e) {
+			e.printStackTrace();
+		}
+
+		if(msg != null)
+			return msg+"\n";
+		else
+			return "Can't "+verb+" "+noun+".\n";
 	}
 
-
-}
+	}}
