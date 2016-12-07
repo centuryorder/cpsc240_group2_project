d285be0c7d428599bf0983d56f9ff894daf6f846
Stephen <Swilson2999@gmail.com>
2016-12-07 14:59:22 -0500

    Add starting check for locked rooms to movement command and made lock check in room class.  Started skeleton of UnlockEvent Class

---
+++ Group_Project_Bork/UnlockEvent.java
@@ -0,0 +1,10 @@
+package Group_Project_Bork;
+import java.util.*;
+/**
+ * @author Stephen Wilson on 12/7/2016.
+*/
+public abstract class UnlockEvent extends Event {
+private boolean locked;
+
+
+}
